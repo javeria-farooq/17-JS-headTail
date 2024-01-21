415726bd4766203ced97f7fa2cd3c78734586728
javeria-farooq <gettingeducation6@gmail.com>
2024-01-21 16:30:35 +0500

    17 JS Assignment

---
+++ app.js
@@ -0,0 +1,17 @@
+// let perc = 73.9
+// document.write(Math.floor(perc))
+
+// document.write(Math.random() * 10)
+
+// let player1 = prompt('Enter heads name')
+// let player2 = prompt('Enter tails name')
+// let toss = Math.random() * 2
+// let change = Math.floor(toss)
+// document.write(change)
+
+// if(change == 0) {
+//     document.write(player1 + ' heads win')
+// }
+// else{
+//     document.write(player2 + ' tails win')
+// }
