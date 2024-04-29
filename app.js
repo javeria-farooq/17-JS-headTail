7061674d21f174cfcfabd3f9e85da14ffa979a0a
javeria-farooq <gettingeducation6@gmail.com>
2024-04-29 18:48:43 +0500

    update javascript file

--- app.js
+++ app.js
@@ -3,15 +3,15 @@
 
 // document.write(Math.random() * 10)
 
-// let player1 = prompt('Enter heads name')
-// let player2 = prompt('Enter tails name')
-// let toss = Math.random() * 2
-// let change = Math.floor(toss)
+let player1 = prompt('Enter heads name')
+let player2 = prompt('Enter tails name')
+let toss = Math.random() * 2
+let change = Math.floor(toss)
 // document.write(change)
 
-// if(change == 0) {
-//     document.write(player1 + ' heads win')
-// }
-// else{
-//     document.write(player2 + ' tails win')
-// }
+if(change == 0) {
+    document.write(player1 + ' heads win')
+}
+else{
+    document.write(player2 + ' tails win')
+}
